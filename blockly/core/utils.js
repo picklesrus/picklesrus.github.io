2a1dd53665fd17987002653821336e0aaf342ad4
Katelyn Mann <picklesrus@users.noreply.github.com>
2017-03-14 17:06:52 -0700

    more testing

--- blockly/core/utils.js
+++ blockly/core/utils.js
@@ -884,5 +884,4 @@ Blockly.utils.runAfterPageLoad = function(fn) {
  */
 Blockly.utils.setCssTransform = function(node, transform) {
   node.style['transform'] = transform;
-  node.style['-webkit-transform'] = transform;
 };
